b24f3390841776ed51acf2f8caf6189970828ed9
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-03-12 18:30:29 +0530

    Create HTMLValidator.h

---
+++ HTMLValidator.h
@@ -0,0 +1,12 @@
+/* Author: Bhavdeep Singh Nijhawan
+   Username: bhavdeepniwp79 */
+
+#ifndef HTMLVALIDATOR_H
+#define HTMLVALIDATOR_H
+
+#include <string>
+#include <vector>
+
+vector<string> validate_html(const string& html_code);
+
+#endif
