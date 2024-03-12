9c734c3f9c29730493b3a09635849b0e1468b74a
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-03-12 18:32:12 +0530

    Create HTMLValidator.cpp

---
+++ HTMLValidator.cpp
@@ -0,0 +1,11 @@
+/* Author: Bhavdeep Singh Nijhawan
+   Username: bhavdeepniwp79 */
+
+#include "HTMLValidator.h"
+#include <regex>
+
+vector<string> validate_html(const tring& html_code) {
+    vector<string> errors;
+
+    return errors;
+}
