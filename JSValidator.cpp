c899e0b01374b84fff932b9df629f42c1c20cb6c
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-03-12 18:36:47 +0530

    Create JSValidator.cpp

---
+++ JSValidator.cpp
@@ -0,0 +1,11 @@
+/* Author: Bhavdeep Singh Nijhawan
+   Username: bhavdeepniwp79 */
+
+#include "JSValidator.h"
+#include <regex>
+
+std::vector<std::string> validate_js(const std::string& js_code) {
+    std::vector<std::string> errors;
+
+    return errors;
+}
