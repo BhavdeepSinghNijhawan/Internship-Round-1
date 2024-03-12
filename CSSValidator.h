44038cc3c225d956b0a2412ad5f67b97aa5e0302
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-03-12 18:33:11 +0530

    Create CSSValidator.h

---
+++ CSSValidator.h
@@ -0,0 +1,12 @@
+/* Author: Bhavdeep Singh Nijhawan
+   Username: bhavdeepniwp79 */
+
+#ifndef CSSVALIDATOR_H
+#define CSSVALIDATOR_H
+
+#include <string>
+#include <vector>
+
+vector<string> validate_css(const string& css_code);
+
+#endif
