8a143c3c7faaba224fd2b9085abcaf60e1121fce
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-03-12 18:35:30 +0530

    Create CSSValidator.cpp

---
+++ CSSValidator.cpp
@@ -0,0 +1,11 @@
+/* Author: Bhavdeep Singh Nijhawan
+   Username: bhavdeepniwp79 */
+
+#include "CSSValidator.h"
+#include <regex>
+
+vector<string> validate_css(const string& css_code) {
+    vector<string> errors;
+
+    return errors;
+}
