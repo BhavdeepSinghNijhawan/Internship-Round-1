b2f882ebee07f553f6d693078af483a81046f85c
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-03-12 18:36:11 +0530

    Create JSValidator.h

---
+++ JSValidator.h
@@ -0,0 +1,12 @@
+/* Author: Bhavdeep Singh Nijhawan
+   Username: bhavdeepniwp79 */
+
+#ifndef JSVALIDATOR_H
+#define JSVALIDATOR_H
+
+#include <string>
+#include <vector>
+
+vector<string> validate_js(const string& js_code);
+
+#endif
